5699f36ffa530ab5041ebf36c9ee43171441aca0
tissa-ctimac <tkhosla@caseytrees.org>
2023-05-15 17:07:15 -0400

    log is busted but trending progress

--- crud.py
+++ crud.py
@@ -1,6 +1,6 @@
 '''handle sql actions'''
 import sqlite3
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Crud:
     '''sql crud'''
@@ -35,3 +35,31 @@ class Crud:
     
     def createLogEntry(self, vals):
         self.crud('INSERT INTO logs (name, start, end) VALUES (?, ?, ?)', vals)
+
+    def readLogs(self):
+        logs = []
+        con = con = sqlite3.connect(self.db)
+        try:
+            cur = con.cursor()
+            for row in cur.execute('SELECT * FROM logs'):
+                xr = []
+                for i, v in enumerate(row):
+                    if not i:
+                        continue
+                    if i == 1:
+                        xr.append(v)
+                    if i == 2:
+                        xr.append(datetime.fromtimestamp(v/1000).strftime("%Y-%m-%d %H:%M:%S"))
+                    if i == 3:
+                        # xr.append(f"{v - row[2]}")
+                        td = timedelta((row[2]/1000) - (v/1000))
+                        d = td.total_seconds()
+                        hh = d.seconds // 3600
+                        mm = (d.seconds % 3600) // 60
+                        ss = d.seconds % 60
+                        xr.append("{:02d}:{:02d}:{:02d}".format(hh, mm, ss))
+                logs.append(xr)
+            con.commit()
+        finally:
+            con.close()
+        return logs
--- main.py
+++ main.py
@@ -12,12 +12,19 @@ app.config.from_object(__name__)
 db = Crud(app.config['DATABASE'])
 
 @app.route("/")
-def index():
-    return render_template('base.html')
+def home():
+    return render_template('home.html')
+
+@app.route("/logs")
+def logs():
+    return render_template('logs.html', logs=db.readLogs())
+
+@app.route("/exes")
+def exes():
+    return render_template('exes.html')
 
 @app.route('/save', methods=['POST'])
 def save():
-    print(request.json)
     db.createLogEntry(
         (request.json['text'],
         request.json['start'],
